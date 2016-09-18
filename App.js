14008ae3a01030c1960d7b8025bf94651f367f44
koenvg <koen.van.geert@gmail.com>
2016-09-18 16:43:46 +0200

    lesson: state

--- App.js
+++ App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            txt: 'this is the state txt',
+            cat: 0
+        }
+    }
+    update(e){
+        this.setState({
+            txt: e.target.value
+        });
+    }
     render(){
-        let text = this.props.txt;
         return (
             <div>
-                <div>Banaan</div>
-                <b> {text} </b>
+                <input type="text" onChange={this.update.bind(this)}/>
+                <h1>{this.state.txt}</h1>
+
             </div>
         )
     }
 }
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-};
 
-App.defaultProps = {
-    txt: 'this is the default value for txt'
-};
-
-ReactDOM.render(
-    <App cat={5} />,
-    document.getElementById('app')
- );
+export default App;
