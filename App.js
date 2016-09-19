5bb5ce4adcc745f0c24ecead07c70ec33907564b
koenvg <koen.van.geert@gmail.com>
2016-09-19 09:19:54 +0200

    component lifecycle: mounting basics

--- App.js
+++ App.js
@@ -1,26 +1,57 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            val: 0
+        };
+        this.update = this.update.bind(this);
+    }
+    update(){
+        this.setState({
+            val: this.state.val + 1
+        })
+    }
+    componentWillMount(){
+        console.log('Mounting!');
+    }
+    componentDidMount(){
+        console.log('mounted!');
+    }
+    componentWillUnmount(){
+        console.log('Bye!');
+    }
+    render(){
+        console.log('rendering!');
+        return(
+            <button onClick={this.update}>{this.state.val}</button>
+        )
+    }
+}
 
+class Wrapper extends React.Component{
+    constructor(){
+        super();
+        this.mount = this.mount.bind(this);
+        this.unmount = this.unmount.bind(this);
+    }
+    mount(){
+        ReactDOM.render(<App/>, document.getElementById('a'));
+    }
+    unmount(){
+        ReactDOM.unmountComponentAtNode(document.getElementById('a'));
+    }
     render(){
         return (
-            <Button> <Icon/> React</Button>
+            <div>
+                <button onClick={this.mount}>Mount</button>
+                <button onClick={this.unmount}>Unmount</button>
+                <div id="a"></div>
+            </div>
         )
     }
 }
 
-class Button extends React.Component {
-    render(){
-        return(
-            <button>{this.props.children}</button>
-        )
-    }
-}
-
-const Icon = () => {
-    return (
-        <span className="glyphicon"></span>
-    )
-};
-
-export default App;
+export default Wrapper;
